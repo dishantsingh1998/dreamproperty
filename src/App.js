29b56164fc4da5885ea7fb44ae7164582166a67b
Dishant Singh <65276766+dishantsingh1998@users.noreply.github.com>
2021-01-26 23:43:55 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import Modal from "./components/Modal";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,112 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "AGARWAL PARAMOUNT",
+    img: "img/product-1.png",
+    price: 2500000,
+    company: "AGARWAL GROUP",
+    info:
+      "1/2/3 BHK AVAILABLE",
+    inCart: false,
+    count: 12,
+    total: 10
+  },
+  {
+    id: 2,
+    title: "JOYVILLE",
+    img: "img/product-2.png",
+    price: 1600000,
+    company: "SHAPOORJI PALLONJI",
+    info:
+      "1/2/3 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "AGARWAL HEIGHTS",
+    img: "img/product-3.png",
+    price: 8000000,
+    company: "AGARWAL GROUP",
+    info:
+      "1/2/3 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "POONAM PARK VIEW",
+    img: "img/product-4.png",
+    price: 1800000,
+    company: "POONAM DEVELOPERS",
+    info:
+      "1/2 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "BACHRAJ LANDMARK",
+    img: "img/product-5.png",
+    price: 24000000,
+    company: "BACHRAJ GROUP",
+    info:
+      "1BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "RACHNA TOWER",
+    img: "img/product-6.png",
+    price: 1700000,
+    company: "GANESH GROUP",
+    info:
+      "1/2/3 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "TWIN TOWER",
+    img: "img/product-7.png",
+    price: 3000000,
+    company: "M.BARIA DEVELOPERS",
+    info:
+      "1/2/3 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "BARON REGENCY",
+    img: "img/product-8.png",
+    price: 200000,
+    company: "BARON REALTY",
+    info:
+      "1/2 BHK AVAILABLE",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "AGARWAL PARAMOUNT",
+  img: "img/product-1.png",
+  price: 10,
+  company: "AGARWAL GROUP",
+  info:
+    "1/2/3 BHK AVAILABLE",
+  inCart: false,
+  count: 10,
+  total: 12
+};
